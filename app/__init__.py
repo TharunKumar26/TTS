be20460689ae8687ea810e21cc475e49af142080
TharunKumar26 <balarajutk26@gmail.com>
2023-04-14 15:52:37 +0530

    fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,36 @@
-from flask import Flask, request, send_file
+from flask import Flask, request, send_file, jsonify
 from gtts import gTTS
 import io
+import asyncio
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "home"
+
 @app.route('/tts', methods=['POST'])
-def tts():
+async def tts():
     try:
-        data = request.get_json()
-        text = data['text']
-        lang = data['lang']
+        data = request.json
+        if not data:
+            return jsonify({'success': False, 'error': 'No data received'})
+
+        text = data.get('text')
+        lang = data.get('lang')
+        if not text or not lang:
+            return jsonify({'success': False, 'error': 'Missing required parameters'})
+
         slow = data.get('slow', False)
 
         output = io.BytesIO()
         gTTS(text=text, lang=lang, slow=slow).write_to_fp(output)
         output.seek(0)
-        
-        return send_file(output, mimetype='audio/mpeg', as_attachment=True, attachment_filename='output.mp3')
+
+        return send_file(output, mimetype='audio/mpeg', download_name=f"{text}.mp3", as_attachment=True)
+
     except Exception as e:
-        return jsonify({'success': False, 'error': str(e)})
+        return jsonify({'success': False, 'error': str(e)}), 400
+
+if __name__ == '__main__':
+    asyncio.run(app.run())
